6a496d1347fb6bb48ca58b82168d51bd34951754
dacronik <dacronik@gmail.com>
2024-04-18 17:21:16 -0400

    agregando dependencias y librerias

---
+++ src/views/HomeView.vue
@@ -0,0 +1,24 @@
+<script setup>
+    
+import {} from 'vue'
+    // props
+
+    // Reactive Values
+
+    // Computed Properties
+
+    // Watchers
+
+    // Lifecycle hooks
+
+</script>
+
+<template>
+    <h1 class="title-home">Hola soy la vista Home</h1>
+</template>
+
+<style scoped>
+.title-home{
+    color: #000;
+}
+</style>

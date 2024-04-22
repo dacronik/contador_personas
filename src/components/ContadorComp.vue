19fbda31307584616b3e7f22f48811d18a8f72a2
dacronik <dacronik@gmail.com>
2024-04-22 00:43:28 -0400

    agregando estilos con tailwind css

--- src/components/ContadorComp.vue
+++ src/components/ContadorComp.vue
@@ -1,5 +1,5 @@
 <script setup>
-import {ref, onMounted, onBeforeUnmount} from 'vue';
+import { ref, onMounted, onBeforeUnmount } from 'vue';
 import { useApiService } from '../stores/apiService.js'
 
 // Reactive Values
@@ -7,20 +7,20 @@ const count = ref(0);
 const apiService = useApiService();
 // Computed Properties
 // Methods
-const sumar = async () =>{
+const sumar = async () => {
     const response = await apiService.enviarComando('manual-add');
-    if (response.success){
+    if (response.success) {
         count.value++;
-    }else{
+    } else {
         console.error(response.message);
     }
 }
-const restar = async () =>{
-    if (count.value > 0){
+const restar = async () => {
+    if (count.value > 0) {
         const response = await apiService.enviarComando('manual-sub');
-        if(response.success){
+        if (response.success) {
             count.value--;
-        }else {
+        } else {
             console.error(response.message);
         }
     }
@@ -28,7 +28,7 @@ const restar = async () =>{
 // Watchers
 
 // Lifecycle hooks
-onMounted(() =>{
+onMounted(() => {
     //apiService.conectarSocketIO();
     console.log(apiService.getContadorManual)
 })
@@ -39,14 +39,21 @@ onBeforeUnmount(() => {
 </script>
 
 <template>
-    <div>
-        <h2>Contador Manual de Personas</h2>
-        <p>Contador: {{ count }}</p>
-        <button @click="sumar">Sumar</button>
-        <button @click="restar">Restar</button>
+    <div class="flex justify-center items-center h-screen bg-gradient-to-br from-red-400 to-blue-400">
+        <div class="bg-gray-200 p-6 rounded-lg">
+            <h2 class=" text-2xl mb-4">Contador Manual de Personas</h2>
+            <div class=" flex justify-center mt-4">
+                <p :class="{ 'text-red-500': count === 0, 'text-green-500': count > 0 }" class=" text-4xl font-bold">{{ count
+                }}</p>
+            </div>
+            <div class="flex justify-center mt-4">
+                <button @click="sumar"
+                    class="px-6 py-2 mr-4 bg-blue-500 text-white rounded-lg hover:bg-blue-600">Sumar</button>
+                <button @click="restar"
+                    class="px-6 py-2 bg-red-500 text-white rounded-lg hover:bg-red-600">Restar</button>
+            </div>
+        </div>
     </div>
 </template>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
